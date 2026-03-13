3610865a3eb45e058496f3f637f7b72682d403e0
Liam Kinne <liam@kinne.io>
2026-03-14 00:31:38 +1000

    fix clippy lints

--- korlan/src/lib.rs
+++ korlan/src/lib.rs
@@ -344,13 +344,11 @@ impl Device {
                 Err(_) => break, // timeout or error -> pipe is empty
                 Ok(b) => {
                     // If this is a reset-ack (command byte 1, opt1=0) we're done
-                    if b.len() >= CMD_MSG_LEN {
-                        if let Some(msg) = CmdMsg::from_bytes(&b) {
-                            if msg.command == CMD_RESET && msg.opt1 == 0 {
+                    if b.len() >= CMD_MSG_LEN
+                        && let Some(msg) = CmdMsg::from_bytes(&b)
+                            && msg.command == CMD_RESET && msg.opt1 == 0 {
                                 break;
                             }
-                        }
-                    }
                 }
             }
         }
